f61c20df28c10c03e401b910e43bd93b654eb049
d_mode <a36092706@gmail.com>
2026-05-03 18:44:14 +0300

    remove hover effect

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    unoptimized: true
-  }
+    unoptimized: true,
+  },
+  sassOptions: {
+    additionalData: `
+      @mixin hover {
+        @media (hover: hover) and (pointer: fine) {
+          &:hover { @content; }
+        }
+      }
+    `,
+  },
 };
 
 export default nextConfig;
